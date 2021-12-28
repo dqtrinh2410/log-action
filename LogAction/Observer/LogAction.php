4bfdabe2ae14f30b66c9f0144cb52bf40412f3d5
Ryan Hoang <ryan.trung@omnyfy.com>
2021-12-28 10:37:52 +0700

    a

--- LogAction/Observer/LogAction.php
+++ LogAction/Observer/LogAction.php
@@ -1,18 +1,18 @@
 <?php
-namespace Changi\PreLogin\Observer;
+namespace Trinh\LogAction\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Controller\ResultFactory;
 
-class CheckLoginObserver implements ObserverInterface
+class LogAction implements ObserverInterface
 {
 
     public function execute(Observer $observer)
     {
         $actionName = $observer->getEvent()->getRequest()->getFullActionName();
-        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/action-log.log');
-        $logger = new \Zend\Log\Logger();
+        $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/Log-Action.log');
+        $logger = new \Zend_Log();
         $logger->addWriter($writer);
         $logger->info($actionName);
     }
